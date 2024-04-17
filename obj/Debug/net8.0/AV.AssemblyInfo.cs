c6995e6361ea383bffbd2f381d7acbcbd60da1f0
Hatem Ben Belgacem <hatem.benbelgacem@outlook.com>
2024-04-17 13:57:57 +0200

    update

--- obj/Debug/net8.0/AV.AssemblyInfo.cs
+++ obj/Debug/net8.0/AV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b79131f465ef6654405260c4b8f5ab9ad2932194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f757ce73380c36b3fb6ca824bd6b25f8ab2c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("AV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
